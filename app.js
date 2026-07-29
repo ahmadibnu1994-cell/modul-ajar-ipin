71112bcb16688811d47d544b9e1c8124e99b533e
ahmadibnu1994-cell <ahmadibnu1994@gmail.com>
2026-07-29 09:10:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 const GEMINI_API_KEY = 'AQ.Ab8RN6LVTKpDDkrA_t6yX7dUV2Dx2MZNfleGqSEv4Czuy-HjJg';
 // =============================================
 
-const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
+const GEMINI_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-exp:generateContent?key=${GEMINI_API_KEY}`;
 
 let currentLevel = 'Sedang';
 
